626aa101317a805b5474364bd8c2b1f6ab70ff2f
C Rahul <crahul@elitmus.com>
2017-05-26 14:15:24 +0530

    changed metadata source url

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures acmesmith'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-source_url 		 'https://commit.elitmus.in/crahul/acmesmith-cookbook' if respond_to?(:source_url)
-issues_url       'https://commit.elitmus.in/crahul/acmesmith-cookbook/issues' if respond_to?(:issues_url)
+source_url 		 'https://github.com/bitsapien/acmesmith-cookbook' if respond_to?(:source_url)
+issues_url       'https://github.com/bitsapien/acmesmith-cookbook/issues' if respond_to?(:issues_url)
